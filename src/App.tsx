280fc01529f842a66f9f83268f1ac543e926dc78
Shantela21 <shantelaslie@gmail.com>
2026-02-09 10:27:19 +0200

    created a loading spinner component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import DataTable from './components/DataTables'
+import LoadingSpinner from './components/LoadingSpinner'
 
 function App() {
 
   return (
     <>
-      <DataTable columns={[]} data={[]} loading={true} />
+      {/* <DataTable columns={[]} data={[]} loading={true} /> */}
+      <LoadingSpinner message="Loading data..." size="large" />
     </>
   )
 }
